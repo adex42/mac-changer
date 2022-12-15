dae9a0c8a00afd6d4ce540eec3c9f2d2864a74a6
adex42 <87116411+adex42@users.noreply.github.com>
2022-12-15 04:13:15 -0500

    Add files via upload

--- mac_changer.py
+++ mac_changer.py
@@ -3,18 +3,19 @@
 import subprocess
 import optparse
 
+def change_mac(interface , new_mac):
+    print(f"[+] Changing MAC of {interface} to  {new_mac}")
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+
 parser = optparse.OptionParser()
 parser.add_option("-i","--interface" , dest="interface" , help="The interface for which the mac is being changed")
 parser.add_option("-m", "--mac", dest="new_mac", help="The new MAC address")
 (options, arguments) = parser.parse_args()
-interface = options.interface
-new_mac = options.new_mac
-print("[+] MAC before change")
-subprocess.call(["ifconfig", interface])
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call(["ifconfig", interface, "up"])
-print("[+] MAC after the Change")
-subprocess.call(["ifconfig", interface])
+change_mac(options.interface,options.new_mac)
+
+
 
 
