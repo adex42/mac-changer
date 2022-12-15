e3b8a1c4a10e525bb8bc8b066f62766c5eac1d28
adex42 <87116411+adex42@users.noreply.github.com>
2022-12-15 03:02:18 -0500

    Add files via upload

--- mac_changer.py
+++ mac_changer.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 
 import subprocess
+import optparse
 
-interface = input("interface >> ")
-new_mac = input("new mac >> ")
+parser = optparse.OptionParser()
+parser.add_option("-i","--interface" , dest="interface" , help="The interface for which the mac is being changed")
+parser.add_option("-m", "--mac", dest="new_mac", help="The new MAC address")
+(options, arguments) = parser.parse_args()
+interface = options.interface
+new_mac = options.new_mac
 print("[+] MAC before change")
 subprocess.call(["ifconfig", interface])
 subprocess.call(["ifconfig", interface, "down"])
